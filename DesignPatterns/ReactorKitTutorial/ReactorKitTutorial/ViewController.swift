b6c7f47dbaf319c55da80eb1fc6bfc1545ecc0c8
zdodev <iver0945@gmail.com>
2021-10-08 12:09:02 +0900

    ⚡📱: IBOutlet 프로퍼티 추가

--- DesignPatterns/ReactorKitTutorial/ReactorKitTutorial/ViewController.swift
+++ DesignPatterns/ReactorKitTutorial/ReactorKitTutorial/ViewController.swift
@@ -1,19 +1,14 @@
-//
-//  ViewController.swift
-//  ReactorKitTutorial
-//
-//  Created by Zero DotOne on 2021/10/07.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
