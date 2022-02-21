e75ab0e2d63afd0914876698cc787c6b827c9333
zdodev <iver0945@gmail.com>
2022-02-21 12:03:52 +0900

    ♻️: 코드 -> 스토리보드 전환

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -2,9 +2,10 @@ import UIKit
 import Combine
 
 class ViewController: UIViewController {
-    private let decreaseButton = UIButton(frame: .zero)
-    private let increaseButton = UIButton(frame: .zero)
-    private let label = UILabel(frame: .zero)
+    @IBOutlet weak var decreaseButton: UIButton!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var IncreaseButton: UIButton!
+    
     private var viewModel = ViewModel()
     private var cancellables = Set<AnyCancellable>()
 
@@ -16,49 +17,20 @@ class ViewController: UIViewController {
     }
     
     private func configureViews() {
-        view.addSubview(decreaseButton)
-        view.addSubview(increaseButton)
-        view.addSubview(label)
-        
-        decreaseButton.translatesAutoresizingMaskIntoConstraints = false
-        decreaseButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        decreaseButton.trailingAnchor.constraint(equalTo: label.leadingAnchor, constant: -20).isActive = true
-        
-        decreaseButton.setTitle("decrease", for: .normal)
-        decreaseButton.setTitleColor(.black, for: .normal)
-        decreaseButton.backgroundColor = .lightGray
-        decreaseButton.addTarget(nil, action: #selector(tappedDecreaseButton), for: .touchUpInside)
-        
-        increaseButton.translatesAutoresizingMaskIntoConstraints = false
-        increaseButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        increaseButton.leadingAnchor.constraint(equalTo: label.trailingAnchor, constant: 20).isActive = true
-        
-        increaseButton.setTitle("increase", for: .normal)
-        increaseButton.setTitleColor(.black, for: .normal)
-        increaseButton.backgroundColor = .lightGray
-        increaseButton.addTarget(nil, action: #selector(tappedIncreaseButton), for: .touchUpInside)
-        
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        label.widthAnchor.constraint(equalToConstant: 50).isActive = true
-        
-        label.text = "label"
-        label.textAlignment = .center
     }
     
     private func bind() {
         viewModel.$count.sink { [weak self] value in
-            self?.label.text = String(value)
+            self?.numberLabel.text = String(value)
         }.store(in: &cancellables)
     }
     
-    @objc func tappedIncreaseButton() {
-        viewModel.increaseCount()
+    @IBAction func tappedDecreaseButton(_ sender: UIButton) {
+        viewModel.decreaseCount()
     }
     
-    @objc func tappedDecreaseButton() {
-        viewModel.decreaseCount()
+    @IBAction func tappedIncreaseButton(_ sender: UIButton) {
+        viewModel.increaseCount()
     }
 }
 
